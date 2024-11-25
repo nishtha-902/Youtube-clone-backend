2a09b99a0b063a2e6c0543db62873b4caa3a1606
nishtha-902 <nishthakathpal902@gmail.com>
2024-11-26 00:16:05 +0530

    connect to mongoDB

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// disturbs consistency as somewhere is import and somewhere is require
+// require('dotenv').config()
+
+//can also load env var directly in dev script in package.json 
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: './env' })
+
+connectDB()
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
